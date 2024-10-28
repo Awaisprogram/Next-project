1479771171ed08ebd7b5176fd511d6324ac256b3
Awais Mehmood <awaisbinmehmoodahmed@gmail.com>
2024-10-28 21:02:00 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import Image from "next/image";
-import section from './images/section.png'
 import Main from './home/page'
 import Features from "./features/page";
 import About from "./about/page";
-import Contact from "./contact/page";
 
 export default function Home() {
   return (
